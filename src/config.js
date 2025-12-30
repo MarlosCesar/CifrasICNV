c4184e0826c2813fcf6b90e616b5865c32575755
MarlosCesar <marloscesar.admop@gmail.com>
2025-12-29 23:34:20 -0300

    Allow config.js for deployment

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+    GOOGLE_DRIVE_FOLDER_ID: "1OzrvB4NCBRTDgMsE_AhQy0b11bdn3v82",
+    GOOGLE_API_KEY: "AIzaSyD2qLxX7fYIMxt34aeWWDsx_nWaSsFCguk",
+    GOOGLE_CLIENT_ID: "977942417278-0mfg7iehelnjfqmk5a32elsr7ll8hkil.apps.googleusercontent.com",
+    GOOGLE_SCOPES: "https://www.googleapis.com/auth/drive.readonly",
+};
+
+export const UI_CONFIG = {
+    FIXED_CATEGORIES: [
+        { name: "Domingo - Manhã", id: "domingo-manha" },
+        { name: "Domingo - Noite", id: "domingo-noite" },
+        { name: "Segunda", id: "segunda" },
+        { name: "Quarta", id: "quarta" }
+    ]
+};
